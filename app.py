bd35c54f0c5e57a5215c649f86770614f1ff9847
Jeevanshi <48143381+Femme-js@users.noreply.github.com>
2020-09-27 22:32:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,17 @@ with open('model.pickle', 'rb') as target:
     model = pickle.load(target)
 
 
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+origin.pull()
+return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
+
+        
 @app.route('/')
 def home():
     return render_template('index.html')
